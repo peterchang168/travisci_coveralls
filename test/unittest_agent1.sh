11deae1ebb78dc367f1337aded504eab10b5edbd
Peter L Chang (RD-TW) <peterl_chang@trend.com.tw>
2016-01-14 18:52:02 +0800

    update path to generate Junit XML report

--- test/unittest_agent1.sh
+++ test/unittest_agent1.sh
@@ -4,8 +4,5 @@ mkdir -p /tmp/agent1_test
 cp ${PWD}/bin/agent1.py /tmp/agent1_test
 cp ${PWD}/test/unittest/unittest_agent1.py /tmp/agent1_test
 mkdir -p /tmp/agent1_test/report
-mkdir $CIRCLE_TEST_REPORTS/junit/
-nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/junit/agent1_unit_result.xml --cover-erase --with-coverage --cover-package=agent1 -w /tmp/agent1_test/ unittest_agent1.py
-junit-viewer --results=$CIRCLE_TEST_REPORTS/junit/agent1_unit_result.xml --save=$CIRCLE_TEST_REPORTS/agent1_unit_result.html
-coverage xml -o $CIRCLE_TEST_REPORTS/junit/agent1_coverage.xml /tmp/agent1_test/agent1.py
-coverage html -d $CIRCLE_TEST_REPORTS/coverage
+nosetests -v -s -x --with-xunit --xunit-file=./agent1_unit_result.xml --cover-erase --with-coverage --cover-package=agent1 -w /tmp/agent1_test/ unittest_agent1.py
+coverage xml -o ./agent1_coverage.xml /tmp/agent1_test/agent1.py
--- test/unittest_agent2.sh
+++ test/unittest_agent2.sh
@@ -3,7 +3,5 @@ mkdir -p /tmp/agent2_test
 cp ${PWD}/bin/agent2.py /tmp/agent2_test
 cp ${PWD}/test/unittest/unittest_agent2.py /tmp/agent2_test
 mkdir -p /tmp/agent2_test/report
-mkdir -p $CIRCLE_TEST_REPORTS/junit/
-nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/junit/agent2_unit_result.xml --cover-erase --with-coverage --cover-package=agent2 -w /tmp/agent2_test/ unittest_agent2.py
-#coverage html -o $CIRCLE_TEST_REPORTS/junit/agent2_coverage.xml /tmp/agent2_test/agent2.py
-coverage html -d $CIRCLE_ARTIFACTS/coverage
+nosetests -v -s -x --with-xunit --xunit-file=./agent2_unit_result.xml --cover-erase --with-coverage --cover-package=agent2 -w /tmp/agent2_test/ unittest_agent2.py
+#coverage html -o ./agent2_coverage.xml /tmp/agent2_test/agent2.py
